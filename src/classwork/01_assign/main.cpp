3e63b39fad0120b5e2741af29f750c48e3e28dd7
Sidrah99 <sidrah.zahid@g.austincc.edu>
2020-09-08 23:48:42 -0500

    Homework 0

--- src/classwork/01_assign/main.cpp
+++ src/classwork/01_assign/main.cpp
@@ -1,11 +1,19 @@
 //Write the include statement for types.h here
-
+#include <iostream>
+#include "types.h"
 //Write include for capturing input from keyboard and displaying output to screen
 
 
 
 int main() 
 {
+	int num, result;
+	std::cin >> num;
+	result = multiply_numbers(num);
+	std::cout << result;
+	int num1 = 4;
+	result = multiply_numbers(num1);
+	std::cout << '\n' << result;
 
 	return 0;
 }
