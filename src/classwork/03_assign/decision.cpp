03df9548377fc97b1dea6b233a6c813643f67b11
Sidrah99 <sidrah.zahid@g.austincc.edu>
2020-09-17 19:01:36 -0500

    Homework 3

--- src/classwork/03_assign/decision.cpp
+++ src/classwork/03_assign/decision.cpp
@@ -1,1 +1,61 @@
+#include "decision.h"
+#include <string>
+
+
 //cpp
+string get_letter_grade_using_if(int grade)
+{
+    if(grade >= 90 && grade <= 100)
+    {
+        return "A";  
+    }
+    else if(grade >= 80 && grade <=89)
+    {
+        return "B";
+    }
+    else if(grade >= 70 && grade <=79)
+    {
+        return "C";
+    }
+    else if(grade >= 60 && grade <=69)
+    {
+       return "D";
+    }
+   else if(grade >= 0 && grade <=59)
+    {
+        return "F";
+    }
+    else 
+    {
+        return "Out of range.";
+    }
+}
+
+string get_letter_grade_using_switch(int grade)
+{
+    string result;
+
+    switch(grade)
+    {
+        case 90 ... 100:
+            result = "A";
+            break;
+        case 80 ... 89:
+            result = "B";
+            break;
+        case 70 ... 79:
+            result = "C";
+            break;
+        case 60 ... 69:
+            result = "D";
+            break;
+        case 0 ... 59:
+            result = "F";
+            break;
+        default:
+            result = "Out of range.";
+            break;
+
+    }
+    return result;
+}   
--- src/classwork/03_assign/main.cpp
+++ src/classwork/03_assign/main.cpp
@@ -1,10 +1,16 @@
 //Write the include statement for decisions.h here
-
+#include <iostream>
+#include "decision.h"
 //Write namespace using statements for cout and cin
 
 int main() 
 {
-
+	int grade;
+	std::cout << "Enter a grade: ";
+	std::cin >> grade;
+	std:: cout << "Your grade is: " << get_letter_grade_using_if(grade) << "\n";
+	std:: cout << "Your grade is: " << get_letter_grade_using_switch(grade);
+	
 	return 0;
 }
 
