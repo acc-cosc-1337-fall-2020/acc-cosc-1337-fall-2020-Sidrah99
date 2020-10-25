a31f0946d2af2e4015ae322ddc18ceb3c838fe63
Sidrah99 <sidrah.zahid@g.austincc.edu>
2020-10-24 19:34:14 -0500

    Homework 5

--- src/homework/tic_tac_toe/main.cpp
+++ src/homework/tic_tac_toe/main.cpp
@@ -1,6 +1,35 @@
+#include "tic_tac_toe.h"
+#include <string>
+#include <vector>
+#include <iostream>
+using std::cout; using std::cin; using std::string; using std::vector;
 
 int main() 
 {
+	TicTacToe game;
+	string entry;
+	int position;
+
+
+	cout << "Player 1, please enter X or O: ";
+	cin >> entry;
+	game.start_game(entry);
+	cout << "\nPlease enter the position on the board you wish to mark (1-9): ";
+	cin >> position;
+	game.mark_board(position);
+	game.display_board();
+
+	while(game.game_over() ==  false)
+	{
+		cout << "\nPlease enter the position on the board you wish to mark (1-9): ";
+		cin >> position;
+		game.mark_board(position);
+		game.display_board();
+	}
+	if(game.game_over() == true)
+	{
+		cout << "Game Over";
+	}
 
 	return 0;
 }
--- src/homework/tic_tac_toe/tic_tac_toe.cpp
+++ src/homework/tic_tac_toe/tic_tac_toe.cpp
@@ -1,1 +1,86 @@
 //cpp
+#include <iostream>
+#include <string>
+#include <vector>
+#include "tic_tac_toe.h"
+using std::cout; using std::cin; using std::string; using std::vector;
+
+bool TicTacToe::game_over()
+{
+    if(check_board_full() == false)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+    
+}
+
+void TicTacToe::start_game(string first_player)
+{
+    clear_board();
+
+    if(first_player == "X" || first_player == "x" || first_player == "O" || first_player == "o")
+    {
+        player = first_player;
+    }
+    else
+    {
+        cout << "Please enter 'X' or 'O'.";
+    }
+}
+
+void TicTacToe::mark_board(int position)
+{
+    pegs[position-1] = player;
+    set_next_player();
+}
+
+void TicTacToe::display_board()const
+{
+    for(int a = 0; a < pegs.size(); a++)
+    {
+        cout << pegs[a] << "| ";
+        if(a == 2 || a == 5 || a == 8)
+        {
+            cout << "\n";
+        }
+    }
+    
+}
+
+void TicTacToe::set_next_player()
+{
+    if(player == "X" || player == "x")
+    {
+        player = "O";
+    }
+    else
+    {
+        player = "X";
+    }
+   
+}
+
+bool TicTacToe::check_board_full()
+{
+    for(int i=0; i < pegs.size(); i++)
+    {
+        if(pegs[i] == " ")
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void TicTacToe::clear_board()
+{
+    for(int i = 0; i < pegs.size() - 1; i++)
+    {
+        pegs[i] = " ";
+    }
+}
